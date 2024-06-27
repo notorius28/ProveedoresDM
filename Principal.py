be25d9ebb62e6ab523d6634cefaa1f1bf78a6af9
Ricardo GP <notorius28@gmail.com>
2024-06-27 20:17:22 +0200

    refactor for prov17

--- Principal.py
+++ Principal.py
@@ -5,13 +5,39 @@ import traceback
 import importlib.util
 from datetime import datetime, timedelta
 import procesadores.funcionesGenericas as fg
-import pandas_xlwt
-import openpyxl
-import tempfile
+import pandas_xlwt #para exportar en xls
 
-# Inicializar el estado si no está presente
-if 'archivo_exportado' not in st.session_state:
-    st.session_state['archivo_exportado'] = False
+
+#Inicializamos variables de sesión
+if "cached_file" not in st.session_state:
+    st.session_state.cached_file = False
+if "excel_dict" not in st.session_state:
+    st.session_state.excel_dict = False
+if 'exported_file' not in st.session_state:
+    st.session_state.exported_file = False
+if 'process_ok' not in st.session_state:
+    st.session_state.process_ok = False
+if 'show_button' not in st.session_state:
+    st.session_state.show_button = False
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+if 'progressbar' not in st.session_state:
+    st.session_state.progressbar = False
+if 'df_procesado_total' not in st.session_state:
+    st.session_state.df_procesado_total = pd.DataFrame()
+if 'df_procesado_sin_formato_total' not in st.session_state:
+    st.session_state.df_procesado_sin_formato_total = pd.DataFrame()
+
+def reiniciar_estados():
+    st.session_state.cached_file = False
+    st.session_state.excel_dict = False
+    st.session_state.exported_file = False
+    st.session_state.process_ok = False
+    st.session_state.show_button = False
+    st.session_state.clicked = False
+    st.session_state.progressbar = False
+    st.session_state.df_procesado_total = pd.DataFrame()
+    st.session_state.df_procesado_sin_formato_total = pd.DataFrame()
 
 # Título de la aplicación y ocupar el ancho completo de página
 st.set_page_config(page_title="ProveedoresDM", layout="wide")
@@ -27,186 +53,140 @@ if procesador_seleccionado == "procesadores/proveedor42.py":
 elif procesador_seleccionado == "procesadores/proveedor23novedades.py":
     st.warning("Para este tipo de fichero, se multiplica por 0,30 el precio original") 
 
-# Función para cargar el archivo
-def cargar_archivo(uploaded_file):
+# Función para cachear el archivo
+@st.cache_data
+def cachear_fichero(file):
+    return file
+
+# Función para leer el fichero (con caché)
+@st.cache_data(show_spinner="Leyendo archivo Excel...")
+def leer_excel(uploaded_file):
     if uploaded_file is not None:
-        with st.spinner('Cargando archivo...'):
-            df = pd.read_excel(uploaded_file)
-            st.session_state['archivo_exportado'] = False
+        df = pd.read_excel(uploaded_file, sheet_name=None)
         return df
     return None
 
 # Función para obtener el número de pestañas del fichero excel 
 def obtener_numero_de_tabs(file):
-    xls = pd.ExcelFile(file)
-    num_hojas = len(xls.sheet_names)
+    num_hojas = len(file)
     return num_hojas
 
-# Función para eliminar pestañas del excel
+def click_boton_procesar_fichero():
+    st.session_state.clicked = True
 
-def eliminar_tabs_excel (file, days):
-    # Cargar el libro de trabajo
-    xls = openpyxl.load_workbook(file)
-    hojas = xls.sheetnames
+# Mostrarmos el botón de importar fichero al cargar el formulario
+ficheroExcel = st.file_uploader("Sube tu archivo Excel", type=["xlsx","xls"], accept_multiple_files=False, on_change=reiniciar_estados)
+st.session_state.cached_file = cachear_fichero(ficheroExcel)
 
-    # Obtener la fecha actual
-    hoy = datetime.today()
-    # Calcular la fecha correspondiente a 30 días antes
-    hace_un_mes = hoy - timedelta(days=days)
+# Si se ha subido el fichero, leemos el excel y lo guardamos en un diccionario
+if st.session_state.cached_file:
+    st.session_state.excel_dict = leer_excel(st.session_state.cached_file)
+    #st.session_state.cached_file = None
 
-    for hoja in hojas:
-        fecha_tab = datetime.strptime(fg.obtener_fecha_desde_texto(hoja),'%Y-%m-%d') 
-        if fecha_tab and fecha_tab < hace_un_mes:
-            del xls[hoja]
-    
-    # Crear un archivo temporal
-    with tempfile.NamedTemporaryFile(delete=False, suffix='.xlsx') as tmp:
-        ruta_excel_temporal = tmp.name
-        # Guardar el archivo modificado en el archivo temporal
-        xls.save(ruta_excel_temporal)
+# Con el fichero excel en memoria, ejecutamos los procesos de transformación
+if st.session_state.excel_dict: 
 
-    return ruta_excel_temporal
-
-# Función para procesar ficheros excel con múltiples pestañas
-def procesar_excel_multiples_tabs(file):
-    xls = pd.ExcelFile(file)
-    hojas = xls.sheet_names
-    num_hojas = len(hojas)
-    df_procesado_total = pd.DataFrame()
-    df_procesado_sin_formato_total = pd.DataFrame()
+    # Miramos el número de pestañas que tiene el fichero
+    numero_tabs = obtener_numero_de_tabs(st.session_state.excel_dict)
+    if numero_tabs > 1: 
+        st.warning(":exclamation: ADVERTENCIA: El fichero cargado tiene más de una pestaña.")
 
     # Importar el procesador seleccionado dinámicamente
     spec = importlib.util.spec_from_file_location(procesador_seleccionado, procesador_seleccionado)
     procesador_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(procesador_module)
 
-    # Creamos una barra de progreso
-    progreso = 0
-    progress_text = str(progreso) + "/" + str(num_hojas) +  " pestañas procesadas"
-    progress_bar = st.progress(0, text=progress_text)
+    # Obtener la fecha actual
+    hoy = datetime.today()
+    # Calcular la fecha correspondiente a 30 días antes
+    hace_un_mes = hoy - timedelta(days=30)
 
-    for hoja in hojas:
-        df_hoja = pd.read_excel(file, sheet_name=hoja)
-        df_procesado, df_procesado_sin_formato = procesador_module.procesarExcel(df_hoja, hoja)
-        df_procesado_total = pd.concat([df_procesado_total, df_procesado], ignore_index=True)
-        df_procesado_sin_formato_total = pd.concat([df_procesado_sin_formato_total, df_procesado_sin_formato], ignore_index=True)
-
-        # Actualizar la barra de progreso
-        progreso += 1
-        progress_text = str(progreso) + "/" + str(num_hojas) +  " pestañas procesadas"
-        progress_bar.progress(progreso / num_hojas, text=progress_text)
-    
-    # Convertimos la fecha de lanzamiento a formato fecha para la exportación, indicando que el día va al principio
-    df_procesado_total['Fecha Lanzamiento'] = pd.to_datetime(df_procesado_total['Fecha Lanzamiento'], dayfirst=True)
-    df_procesado_sin_formato_total['Fecha Lanzamiento'] = pd.to_datetime(df_procesado_sin_formato_total['Fecha Lanzamiento'], dayfirst=True)
-
-    #Convertimos la fecha de lanzamiento a formato dd/mm/YYYY
-    df_procesado_total['Fecha Lanzamiento'] = df_procesado_total['Fecha Lanzamiento'].dt.strftime('%d-%m-%Y')
-    df_procesado_sin_formato_total['Fecha Lanzamiento'] = df_procesado_sin_formato_total['Fecha Lanzamiento'].dt.strftime('%d-%m-%Y')
-    st.write("Los resultados generados corresponden sólo a los lanzamientos de los últimos 30 días")
-    return df_procesado_total, df_procesado_sin_formato_total
-
-# Subir el archivo
-uploaded_file = st.file_uploader("Sube tu archivo Excel", type=["xlsx","xls"])
-
-# Mostrar el DataFrame y ejecutar el procesamiento
-if uploaded_file:
-
-    # Miramos el número de pestañas que tiene el fichero
-    numero_tabs = obtener_numero_de_tabs(uploaded_file)
-    
-    if numero_tabs == 1:
-        df = cargar_archivo(uploaded_file)
-        if df is not None:
+    # En un bucle, leemos cada pestaña del excel
+    for nombre, hoja in st.session_state.excel_dict.items():
+        
+        if numero_tabs > 1:
+            # En multipestaña, obtenemos la fecha del nombre de la pestaña y no mostramos datos previso
+            fecha_tab = datetime.strptime(fg.obtener_fecha_desde_texto(nombre),'%Y-%m-%d') 
+        else:
+            # Con una sola pestaña, mostramos la vista previa y el total de registros
+            fecha_tab = hoy
             st.write("Aquí están los datos del fichero original:")
-            st.dataframe(df)
-            num_filas = df.shape[0]
+            st.dataframe(hoja)
+            num_filas = hoja.shape[0]
             st.write("El número de filas del fichero original es **" + str(num_filas)+"**.")
+        
+        # Mostramos el botón para procesar el fichero
+        if not st.session_state.show_button:
+            st.button("Procesar Fichero", on_click=click_boton_procesar_fichero)
+            st.session_state.show_button = True
+
+        # Procesamos el fichero si no se ha exportado previamente
+        if st.session_state.clicked and st.session_state.exported_file == False:
+
+            # Mostramos una barra de progreso
+            if not st.session_state.progressbar:
+                progreso = 0
+                progress_text = str(progreso) + "/" + str(numero_tabs) +  " pestañas procesadas"
+                progress_bar = st.progress(progreso / numero_tabs, text=progress_text)
+                st.session_state.progressbar = True
             
-            # Botón para procesar el DataFrame
-            if st.button("Procesar Fichero"):
-                try:
-                    # Importar el procesador seleccionado dinámicamente
-                    spec = importlib.util.spec_from_file_location(procesador_seleccionado, procesador_seleccionado)
-                    procesador_module = importlib.util.module_from_spec(spec)
-                    spec.loader.exec_module(procesador_module)
-                    
-                    # Llamar a la función procesarExcel y mostrar el DataFrame procesado
-                    df_procesado, df_procesado_sin_formato = procesador_module.procesarExcel(df)
-                    st.write("Fichero procesado:")
-                    st.dataframe(df_procesado)
-
-                    st.write("Filas sin formato equivalente:")
-                    st.dataframe(df_procesado_sin_formato)
-                    
-                    #Exportamos el fichero en formato excel 97/2000 (XLS)
-                    df_procesado.to_excel('archivo_procesado.xls',engine='xlwt',index=False)
-                    num_filas = df_procesado.shape[0]
-                    st.success("El fichero procesado ha sido exportado exitosamente. Total de filas: **"+ str(num_filas) +"**")
-                    st.session_state.archivo_exportado = True
-                except Exception as e:
-                    st.error(f"¡Error al procesar el fichero: {str(e)}")
-                    st.error(traceback.format_exc())
-
-            # Mostrar el botón de descarga si el archivo ha sido exportado
-            if st.session_state['archivo_exportado']:
-                st.markdown("### Pulsa este botón para descargar el archivo procesado y exportado")
-                with open("archivo_procesado.xls", "rb") as file:
-                    
-                    # Renombramos el fichero 
-                    now = datetime.now()
-                    timestamp = now.strftime("%Y%m%d_%H%M%S")
-                    filename_out = f'procesado_{timestamp}.xls'
-
-                    # Botón de descarga
-                    st.download_button(
-                        label="Descargar Excel Procesado",
-                        data=file,
-                        file_name=filename_out,
-                        mime="application/vnd.ms-excel."
-                    )
-    else:
-        st.warning(":exclamation: ADVERTENCIA: El fichero cargado tiene más de una pestaña. La aplicación está en pruebas aún para procesar este tipo de ficheros, por lo que los resultados pueden ser erróneos.")
-
-        # Botón para procesar el DataFrame
-        if st.button("Procesar Fichero"):
             try:
-                st.write("Eliminando las pestañas del excel con antigüedad superior a 30 días (es un proceso lento)...")
-                # Primero limpiamos las pestañas antiguas
-                ruta_excel_limpio = eliminar_tabs_excel(uploaded_file, 30)
-                del uploaded_file
+                # Comprobamos que la pestaña sea de una fecha reciente y procesamos el excel, almacenando los resultados en dos dataframes                             
+                if fecha_tab >= hace_un_mes:
+                    df_procesado, df_procesado_sin_formato = procesador_module.procesarExcel(hoja, nombre)
+                    st.session_state.df_procesado_total = pd.concat([st.session_state.df_procesado_total, df_procesado], ignore_index=True)
+                    st.session_state.df_procesado_sin_formato_total = pd.concat([st.session_state.df_procesado_sin_formato_total, df_procesado_sin_formato], ignore_index=True)
 
-                st.write("Procesando fichero con múltiples pestañas...")
-                df_procesado, df_procesado_sin_formato =procesar_excel_multiples_tabs(ruta_excel_limpio)
-                st.write("Fichero procesado:")
-                st.dataframe(df_procesado)
+                # Actualizar la barra de progreso
+                progreso += 1
+                progress_text = str(progreso) + "/" + str(numero_tabs) +  " pestañas procesadas"
+                progress_bar.progress(progreso / numero_tabs, text=progress_text)
 
-                st.write("Filas sin formato equivalente:")
-                st.dataframe(df_procesado_sin_formato)
-                
-                #Exportamos el fichero en formato excel 97/2000 (XLS)
-                df_procesado.to_excel('archivo_procesado.xls',engine='xlwt',index=False)
-                num_filas = df_procesado.shape[0]
-                st.success("El fichero procesado ha sido exportado exitosamente. Total de filas: **"+ str(num_filas) +"**")
-                st.session_state.archivo_exportado = True
+                if progreso == numero_tabs:
+                    # Convertimos la fecha de lanzamiento a formato fecha para la exportación, indicando que el día va al principio
+                    st.session_state.df_procesado_total['Fecha Lanzamiento'] = pd.to_datetime(st.session_state.df_procesado_total['Fecha Lanzamiento'], dayfirst=True)
+                    st.session_state.df_procesado_sin_formato_total['Fecha Lanzamiento'] = pd.to_datetime(st.session_state.df_procesado_sin_formato_total['Fecha Lanzamiento'], dayfirst=True)
+
+                    # Convertimos la fecha de lanzamiento a formato dd/mm/YYYY
+                    st.session_state.df_procesado_total['Fecha Lanzamiento'] = st.session_state.df_procesado_total['Fecha Lanzamiento'].dt.strftime('%d-%m-%Y')
+                    st.session_state.df_procesado_sin_formato_total['Fecha Lanzamiento'] = st.session_state.df_procesado_sin_formato_total['Fecha Lanzamiento'].dt.strftime('%d-%m-%Y')
+
+                    # Exportamos el fichero en formato excel 97/2000 (XLS)
+                    st.session_state.df_procesado_total.to_excel('archivo_procesado.xls',engine='xlwt',index=False)
+                    st.session_state.exported_file = True
+                    st.session_state.process_ok = True
+
             except Exception as e:
                 st.error(f"¡Error al procesar el fichero: {str(e)}")
                 st.error(traceback.format_exc())
 
-        # Mostrar el botón de descarga si el archivo ha sido exportado
-        if st.session_state['archivo_exportado']:
-            st.markdown("### Pulsa este botón para descargar el archivo procesado y exportado")
-            with open("archivo_procesado.xls", "rb") as file:
-                
-                # Renombramos el fichero 
-                now = datetime.now()
-                timestamp = now.strftime("%Y%m%d_%H%M%S")
-                filename_out = f'procesado_{timestamp}.xls'
+    # Si todo ha ido bien, mostramos los resultados en pantalla
+    if st.session_state.process_ok:
+        st.write("Fichero procesado:")
+        st.dataframe(st.session_state.df_procesado_total)
 
-                # Botón de descarga
-                st.download_button(
-                    label="Descargar Excel Procesado",
-                    data=file,
-                    file_name=filename_out,
-                    mime="application/vnd.ms-excel."
+        st.write("Filas sin formato equivalente:")
+        st.dataframe(st.session_state.df_procesado_sin_formato_total)
+
+        num_filas = st.session_state.df_procesado_total.shape[0]
+        st.success("El fichero procesado ha sido exportado exitosamente. Total de filas: **"+ str(num_filas) +"**")
+
+    # Mostrar el botón de descarga si el archivo ha sido exportado
+    if st.session_state.exported_file:
+        st.markdown("### Pulsa este botón para descargar el archivo procesado y exportado")
+        with open("archivo_procesado.xls", "rb") as file:
+            
+            # Renombramos el fichero 
+            now = datetime.now()
+            timestamp = now.strftime("%Y%m%d_%H%M%S")
+            filename_out = f'procesado_{timestamp}.xls'
+
+            # Botón de descarga
+            st.download_button(
+                label="Descargar Excel Procesado",
+                data=file,
+                file_name=filename_out,
+                mime="application/vnd.ms-excel."
                 )
+        
+
